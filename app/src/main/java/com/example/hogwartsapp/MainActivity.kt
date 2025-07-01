cc36a99f84e3c9c48828a47c72c9b397c3284543
nathieli <nathielileal123@gmail.com>
2025-06-30 23:40:15 -0300

    fix: tema

--- app/src/main/java/com/example/hogwartsapp/MainActivity.kt
+++ app/src/main/java/com/example/hogwartsapp/MainActivity.kt
@@ -4,17 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
 
         findViewById<Button>(R.id.btnPersonagem).setOnClickListener {
             startActivity(Intent(this, PersonagemEspecificoActivity::class.java))
